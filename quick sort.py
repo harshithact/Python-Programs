117031e8375e80cff9fe43bc46766c7cf1a08660
Harshitha Chovanda <113956065+harshithact@users.noreply.github.com>
2023-02-05 21:27:19 +0530

    Add files via upload

---
+++ quick sort.py
@@ -0,0 +1,21 @@
+def quick_sort(arr, low, high):
+    if low < high:
+        pivot = partition(arr, low, high)
+        quick_sort(arr, low, pivot - 1)
+        quick_sort(arr, pivot + 1, high)
+
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+# test the algorithm with an example array
+arr = [12, 11, 13, 5, 6]
+n = len(arr)
+quick_sort(arr, 0, n - 1)
+print("Sorted array is:", arr)
