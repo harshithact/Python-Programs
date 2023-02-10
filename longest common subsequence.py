97a86f98b1145dc4b71ab19b4671444668be438b
Harshitha Chovanda <113956065+harshithact@users.noreply.github.com>
2023-02-10 20:07:14 +0530

    Update longest common subsequence.py

--- longest common subsequence.py
+++ longest common subsequence.py
@@ -1,4 +1,5 @@
 def LCS(X, Y):
+    
     m = len(X)
     n = len(Y)
     L = [[0] * (n + 1) for i in range(m + 1)]
@@ -20,9 +21,11 @@ def LCS(X, Y):
     while i > 0 and j > 0:
         if X[i - 1] == Y[j - 1]:
             lcs[index - 1] = X[i - 1]
+            
             i -= 1
             j -= 1
             index -= 1
+            
         elif L[i - 1][j] > L[i][j - 1]:
             i -= 1
         else:
