0f27044774ccab1ba4b9aa316c72e24905e0d96f
Harshitha Chovanda <113956065+harshithact@users.noreply.github.com>
2023-02-14 19:39:53 +0530

    Add files via upload

---
+++ Fractional knapsack problem.py
@@ -0,0 +1,11 @@
+def fractional_knapsack(value, weight, capacity):
+    items = list(zip(value, weight))
+    items.sort(key=lambda x: x[0] / x[1], reverse=True)
+    total_value = 0
+    for v, w in items:
+        if capacity == 0:
+            break
+        amount = min(w, capacity)
+        total_value += amount * (v / w)
+        capacity -= amount
+    return total_value
