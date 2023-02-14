c5c8ecad828bdfb65acf93aeac84c88f43392dca
Harshitha Chovanda <113956065+harshithact@users.noreply.github.com>
2023-02-14 19:39:28 +0530

    Add files via upload

---
+++ Activity selection problem.py
@@ -0,0 +1,9 @@
+def activity_selection(start, finish):
+    n = len(start)
+    activities = list(zip(start, finish))
+    activities.sort(key=lambda x: x[1])
+    selected = [activities[0]]
+    for i in range(1, n):
+        if activities[i][0] >= selected[-1][1]:
+            selected.append(activities[i])
+    return selected
