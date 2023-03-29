0e670c6191dee92537ceaa879f02432e8b56851e
Harshitha Chovanda <113956065+harshithact@users.noreply.github.com>
2023-03-29 11:32:19 +0530

    Update Activity selection problem.py

--- Activity selection problem.py
+++ Activity selection problem.py
@@ -4,6 +4,7 @@ def activity_selection(start, finish):
     activities = list(zip(start, finish))
     activities.sort(key=lambda x: x[1])
     selected = [activities[0]]
+    
     for i in range(1, n):
         if activities[i][0] >= selected[-1][1]:
             selected.append(activities[i])
