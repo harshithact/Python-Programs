e48abd3e9626e5478bdf8c069717d71d5e0482dc
Harshitha Chovanda <113956065+harshithact@users.noreply.github.com>
2023-03-30 19:33:16 +0530

    Update breadth first search.py

--- breadth first search.py
+++ breadth first search.py
@@ -20,7 +20,6 @@ class Graph:
                 if not visited[i]:
                     queue.append(i)
                     visited[i] = True
-
 g = Graph()
 g.addEdge(0, 1)
 g.addEdge(0, 2)
@@ -28,7 +27,6 @@ g.addEdge(1, 2)
 g.addEdge(2, 0)
 g.addEdge(2, 3)
 g.addEdge(3, 3)
-
 print("Following is Breadth First Traversal"
       " (starting from vertex 2)")
 g.BFS(2)
